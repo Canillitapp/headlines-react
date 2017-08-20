552c4003f685b8490ed615c804e53c70f0b08671
Alex <1187547+alexruzzarin@users.noreply.github.com>
2017-08-20 18:30:46 -0300

    feat(nav): add react-navigation and Material UI, remove native-base (#7)

--- index.android.js
+++ index.android.js
@@ -2,4 +2,6 @@ import { AppRegistry } from 'react-native';
 
 import Main from './src/Main';
 
+export default Main;
+
 AppRegistry.registerComponent('HeadlinesReact', () => Main);
--- src/Main.js
+++ src/Main.js
@@ -1,105 +1,58 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import {
-  StyleProvider,
-  Container,
-  Content,
-  Right,
-  Title,
-  Tab,
-  Tabs,
-  TabHeading,
-  Text
-} from 'native-base';
+
+import { ThemeProvider } from 'react-native-material-ui';
+import { TabNavigator } from 'react-navigation';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-import getTheme from '../native-base-theme/components';
-import material from '../native-base-theme/variables/material';
-import AppHeader from './components/AppHeader';
 import Highlights from './components/Highlights';
 
-export default function Main() {
-  const tabTitle = (title, icon) =>
-    <TabHeading>
-      <Icon name={icon} color="#fff" size={20} />
-      <Text style={styles.tabText}>
-        {title}
-      </Text>
-    </TabHeading>;
+const AppNav = TabNavigator(
+  {
+    TrendingTab: {
+      screen: Highlights,
+      path: '/',
+      navigationOptions: {
+        title: 'Trending',
+        tabBarLabel: 'Destacados',
+        tabBarIcon: () => <Icon name="newspaper-o" color="#fff" size={20} />
+      }
+    },
+    PopularTab: {
+      screen: Highlights,
+      path: '/popular',
+      navigationOptions: {
+        title: 'Popular',
+        tabBarLabel: 'Popular',
+        tabBarIcon: () => <Icon name="line-chart" color="#fff" size={20} />
+      }
+    },
+    LatestTab: {
+      screen: Highlights,
+      path: '/latest',
+      navigationOptions: {
+        title: 'Reciente',
+        tabBarLabel: 'Reciente',
+        tabBarIcon: () => <Icon name="history" color="#fff" size={20} />
+      }
+    },
+    ReactionsTab: {
+      screen: Highlights,
+      path: '/reactions',
+      navigationOptions: {
+        title: 'Reacciones',
+        tabBarLabel: 'Reacciones',
+        tabBarIcon: () => <Icon name="heart-o" color="#fff" size={20} />
+      }
+    }
+  },
+  {}
+);
 
+export default function Main() {
   return (
-    <StyleProvider style={getTheme(material)}>
-      <Container>
-        <Tabs initialPage={0}>
-          <Tab heading={tabTitle('Destacados', 'newspaper-o')}>
-            <Highlights />
-          </Tab>
-          <Tab heading={tabTitle('Popular', 'line-chart')}>
-            <Content>
-              <View style={styles.container}>
-                <Text style={styles.welcome}>Tab2!</Text>
-                <Text style={styles.instructions}>
-                  To get started, edit index.android.js
-                </Text>
-                <Text style={styles.instructions}>
-                  Double tap R on your keyboard to reload,{'\n'}
-                  Shake or press menu button for dev menu
-                </Text>
-              </View>
-            </Content>
-          </Tab>
-          <Tab heading={tabTitle('Reciente', 'history')}>
-            <Content>
-              <View style={styles.container}>
-                <Text style={styles.welcome}>Tab3!</Text>
-                <Text style={styles.instructions}>
-                  To get started, edit index.android.js
-                </Text>
-                <Text style={styles.instructions}>
-                  Double tap R on your keyboard to reload,{'\n'}
-                  Shake or press menu button for dev menu
-                </Text>
-              </View>
-            </Content>
-          </Tab>
-          <Tab heading={tabTitle('Reacciones', 'heart-o')}>
-            <Content>
-              <View style={styles.container}>
-                <Text style={styles.welcome}>Tab3!</Text>
-                <Text style={styles.instructions}>
-                  To get started, edit index.android.js
-                </Text>
-                <Text style={styles.instructions}>
-                  Double tap R on your keyboard to reload,{'\n'}
-                  Shake or press menu button for dev menu
-                </Text>
-              </View>
-            </Content>
-          </Tab>
-        </Tabs>
-      </Container>
-    </StyleProvider>
+    <ThemeProvider>
+      <AppNav />
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
-  },
-  tabText: {
-    fontSize: 12
-  }
-});
