6ff9b55eac39ca1cc7f093b8b3ac4a0ed9b8b81d
Alex <1187547+alexruzzarin@users.noreply.github.com>
2017-08-21 19:00:03 -0300

    feat(nav): try a custom header toolbar with search

---
+++ src/mocks/latest.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    news_id: 132240,
+    url:
+      'http://tn.com.ar/musica/hoy/encontraron-muerto-walter-romero-el-excantante-de-banda-xxi_784972',
+    title: 'Encontraron muerto a Walter Romero, el excantante de Banda XXI',
+    date: 1491704606,
+    source_id: 1,
+    img_url:
+      'http://cdn.tn.com.ar/sites/default/files/styles/470x269/public/2017/04/08/1266581830507_f.jpg',
+    source_name: 'TN',
+    reactions: []
+  },
+  {
+    news_id: 132238,
+    url:
+      'http://www.telam.com.ar/notas/201704/185120-futsal-argentina-brasil-copa-amarica-de-san-juan.html',
+    title: 'Argentina y Brasil empataron en la Copa América de San Juan',
+    date: 1491703140,
+    source_id: 10,
+    img_url:
+      'http://www.telam.com.ar/advf/imagenes/2017/04/58e9963caf3f5_400x225.jpg',
+    source_name: 'Telam',
+    reactions: []
+  }
+];
---
+++ src/mocks/search.js
@@ -0,0 +1,39 @@
+export default [
+  {
+    news_id: 134751,
+    url:
+      'http://www.perfil.com//politica/en-la-carpa-itinerante-docente-venden-remeras-con-la-leyenda-macri-gato.phtml',
+    title:
+      'En la carpa de los docentes se venden remeras con la leyenda "Macri gato"',
+    date: 1492108721,
+    source_id: 8,
+    img_url:
+      'http://www.perfil.com/orinoco-files/media/images/raw/2017/04/13/0413remeramacrigatog.jpg',
+    source_name: 'Perfil',
+    reactions: []
+  },
+  {
+    news_id: 94409,
+    url:
+      'http://www.perfil.com//politica/hackearon-la-cuenta-de-twitter-de-patricia-bullrich.phtml',
+    title: '“Macri Gato”: hackearon la cuenta de Twitter de Patricia Bullrich',
+    date: 1485461250,
+    source_id: 8,
+    img_url:
+      'http://www.perfil.com/orinoco-files/media/images/raw/2017/01/26/0126patriciabullrichtuitsg2.jpg',
+    source_name: 'Perfil',
+    reactions: []
+  },
+  {
+    news_id: 94363,
+    url:
+      'http://tn.com.ar/politica/le-hackearon-el-twitter-patricia-bullrich-macri-gato_768717',
+    title: 'Escandaloso hackeo del Twitter de Patricia Bullrich: "Macri gato"',
+    date: 1485457233,
+    source_id: 1,
+    img_url:
+      'http://cdn.tn.com.ar/sites/default/files/styles/470x269/public/2017/01/26/bullrich-va.jpg',
+    source_name: 'TN',
+    reactions: []
+  }
+];
