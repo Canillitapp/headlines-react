8eeead6cd8f173b4a1a59742489d7ba36548562d
Alex <1187547+alexruzzarin@users.noreply.github.com>
2017-09-10 18:43:24 -0300

    chore(deps): upgrade to @cerebral/react

--- src/screens/PopularItem.js
+++ src/screens/PopularItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { connect } from 'cerebral/react';
+import { connect } from '@cerebral/react';
 import { state, props } from 'cerebral/tags';
 
 import NewsItem from '../components/NewsItem';
--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { connect } from 'cerebral/react';
+import { connect } from '@cerebral/react';
 import { state } from 'cerebral/tags';
 import { StyleSheet, Image, View, Dimensions } from 'react-native';
 import { RkText, RkTheme } from 'react-native-ui-kitten';
