effa3dc6db84cad11ba23323f38ed272a5049b73
Alex <1187547+alexruzzarin@users.noreply.github.com>
2017-12-10 04:50:21 -0300

    fix download key env

--- scripts/download_keystore.sh
+++ scripts/download_keystore.sh
@@ -1,11 +1,11 @@
 # use curl to download a keystore from $KEY_URL, if set,
 # to the path/filename set in $KEYSTORE.
+KEYSTORE="/root/repo/android/app/${CANILLITA_RELEASE_STORE_FILE}"
+
 if [[ $KEYSTORE && ${KEYSTORE} && $KEY_URL && ${KEY_URL} ]]
 then
     echo "Keystore detected - downloading..."
-    # we're using curl instead of wget because it will not
-    # expose the sensitive uri in the build logs:
     curl -L -o ${KEYSTORE} ${KEY_URL}
 else
-    echo "Keystore uri not set.  .APK artifact will not be signed."
+    echo "Keystore uri not set. '.APK' artifact will not be signed."
 fi
