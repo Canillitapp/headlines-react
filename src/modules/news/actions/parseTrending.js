e152da100e381fd4ca9b61aa66711512353cb3f6
Alex <1187547+alexruzzarin@users.noreply.github.com>
2018-02-09 18:53:25 -0500

    chore: fix eslint errors (#215)

--- src/modules/news/actions/parseTrending.js
+++ src/modules/news/actions/parseTrending.js
@@ -9,20 +9,19 @@ export default function parseTrending({ props, state }) {
     allTopics: result.keywords.map(k => `${k}-${date}`)
   };
   for (let key in result.news) {
-    if (!result.news.hasOwnProperty(key)) {
-      return;
+    if (result.news.hasOwnProperty(key)) {
+      const news = result.news[key];
+      const newsKeys = [];
+      news.forEach(n => {
+        newsKeys.push(n.news_id);
+        output.news[n.news_id] = n;
+      });
+      output.topics[`${key}-${date}`] = {
+        topic: key,
+        date,
+        news: newsKeys
+      };
     }
-    const news = result.news[key];
-    const newsKeys = [];
-    news.forEach(n => {
-      newsKeys.push(n.news_id);
-      output.news[n.news_id] = n;
-    });
-    output.topics[`${key}-${date}`] = {
-      topic: key,
-      date,
-      news: newsKeys
-    };
   }
 
   return output;
