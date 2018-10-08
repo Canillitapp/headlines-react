d00643f7dc504f18efc9975630f72e2c629df381
Alex <1187547+alexruzzarin@users.noreply.github.com>
2018-10-08 02:30:48 -0300

    fix "tests"

--- __mocks__/RNI18n.js
+++ __mocks__/RNI18n.js
@@ -1,6 +1,5 @@
 import { NativeModules } from 'react-native';
 
-NativeModules.RNI18n = {
-  languages: ['en-US'],
-  getLanguages: jest.fn(() => Promise.resolve({}))
+NativeModules.RNLanguages = {
+  languages: ['en-US']
 };
